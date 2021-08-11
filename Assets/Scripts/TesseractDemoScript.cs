23ccbb0c08364f9868d1d9d80e5d2099ecd55b8c
Lucca Giffoni <luccagiffoni@outlook.com>
2021-08-11 10:46:34 -0300

    First commit

---
+++ Assets/Scripts/TesseractDemoScript.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+using UnityEngine.Windows.WebCam;
+using UnityEditor;
+using System.Threading;
+#if ENABLE_WINMD_SUPPORT
+using UnityEngine.XR.WSA.WebCam;
+#endif
+
+public class TesseractDemoScript : MonoBehaviour
+{
+    // OCR Functions
+    [SerializeField] private Texture2D imageToRecognize;
+    [SerializeField] private Text displayText;
+    [SerializeField] private RawImage outputImage;
+    private TesseractDriver _tesseractDriver;
+    private string _text = "";
+
+
+    private Texture2D _texture;
+    private Texture2D snap;
+    private WebCamTexture _webcamTexture;
+
+    [SerializeField] RawImage image;
+
+    // Capturing the screen
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+    public static TesseractDemoScript instance;
+
+    public void Start()
+    {
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Create a PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                // Take a picture
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+
+        Texture2D mainTexture = DuplicateTexture(targetTexture);
+
+        _tesseractDriver = new TesseractDriver();
+         Recoginze(mainTexture);
+    }
+
+    private Texture2D DuplicateTexture(Texture2D snap)
+    {
+        // Create a temporary RenderTexture of the same size as the texture
+        RenderTexture tmp = RenderTexture.GetTemporary(
+                            snap.width,
+                            snap.height,
+                            0,
+                            RenderTextureFormat.Default,
+                            RenderTextureReadWrite.Linear);
+
+        // Blit the pixels on texture to the RenderTexture
+        Graphics.Blit(snap, tmp);
+
+        // Backup the currently set RenderTexture
+        RenderTexture previous = RenderTexture.active;
+
+        // Set the current RenderTexture to the temporary one we created
+        RenderTexture.active = tmp;
+
+        // Create a new readable Texture2D to copy the pixels to it
+        Texture2D myTexture2D = new Texture2D(snap.width, snap.height);
+
+        // Copy the pixels from the RenderTexture to the new Texture
+        myTexture2D.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+        myTexture2D.Apply();
+
+        // Reset the active RenderTexture
+        RenderTexture.active = previous;
+
+        // Release the temporary RenderTexture
+        RenderTexture.ReleaseTemporary(tmp);
+
+        // "myTexture2D" now has the same pixels from "texture" and it's readable.
+        return myTexture2D;
+    }
+
+    private void Recoginze(Texture2D outputTexture)
+    {
+        _texture = outputTexture;
+        ClearTextDisplay();
+        AddToTextDisplay(_tesseractDriver.CheckTessVersion());
+        _tesseractDriver.Setup(OnSetupCompleteRecognize);
+    }
+
+    private void OnSetupCompleteRecognize()
+    {
+        AddToTextDisplay(_tesseractDriver.Recognize(_texture));
+        AddToTextDisplay(_tesseractDriver.GetErrorMessage(), true);
+        SetImageDisplay();
+    }
+
+    private void ClearTextDisplay()
+    {
+        _text = "";
+    }
+
+    private void AddToTextDisplay(string text, bool isError = false)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return;
+
+        _text += (string.IsNullOrWhiteSpace(displayText.text) ? "" : "\n") + text;
+
+        if (isError)
+            Debug.LogError("Ocorreu um erro durante a transcricao do texto para a tela");
+        else
+            Debug.Log("Deu tudo certo: " + text);
+
+        displayText.text = _text;
+    }
+
+    private void Update()
+    {
+        displayText.text = _text;
+    }
+
+    private void SetImageDisplay()
+    {
+        RectTransform rectTransform = outputImage.GetComponent<RectTransform>();
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+            rectTransform.rect.width * _tesseractDriver.GetHighlightedTexture().height / _tesseractDriver.GetHighlightedTexture().width);
+        outputImage.texture = _tesseractDriver.GetHighlightedTexture();
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        // Copy the raw image data into our target texture
+        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+        // Create a gameobject that we can apply our texture to
+        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad.transform.localScale = new Vector3(3, 3, 3);
+        Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+        quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+
+        quad.transform.parent = this.transform;
+        quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+
+        quadRenderer.material.SetTexture("_MainTex", targetTexture);
+
+        // Deactivate our camera
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Shutdown our photo capture resource
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+}
